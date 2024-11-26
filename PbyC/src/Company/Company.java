ba70a9f310b71464786e91d89143dd989c8c214b
sdac-23 <sdac-23@sdac-23-PC>
2024-11-26 09:29:01 +0530

    Company

---
+++ PbyC/src/Company/Company.java
@@ -0,0 +1,23 @@
+package Company;
+
+import Policy.Policy;
+
+public abstract class Company implements Policy {
+
+	@Override
+	public void calSal(int salary, int bonus) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void calSal1(int salary, int bonus, int tax) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void calSal2(int salary) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
