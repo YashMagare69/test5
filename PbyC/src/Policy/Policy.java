aee9f2829ae59969ff2a72835dd8caea6187d649
sdac-23 <sdac-23@sdac-23-PC>
2024-11-26 09:26:28 +0530

    Policy

---
+++ PbyC/src/Policy/Policy.java
@@ -0,0 +1,15 @@
+package Policy;
+
+public interface Policy {
+
+	public void calSal(int salary,int bonus);
+	
+	public void calSal1(int salary,int bonus,int tax);
+	
+	public void calSal2(int salary);
+
+
+
+
+	
+}
